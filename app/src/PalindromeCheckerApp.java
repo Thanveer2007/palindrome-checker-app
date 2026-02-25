b03f8b70a26026a262e0a9877f2b51ef10833781
Thanveer Asshief <asshiefasshief@gmail.com>
2026-02-25 14:50:53 +0530

    uc1 - pr printing welcome message

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("version : 1.0");
+        System.out.println("System initialized sucessfully");
+
+    }
+}
