8256a00e6fa4617983ed2d2137bfd838df86f899
Thanveer Asshief <asshiefasshief@gmail.com>
2026-02-25 16:02:26 +0530

    uc4-array palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -4,6 +4,7 @@
 
 
 
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -11,15 +12,26 @@ public class PalindromeCheckerApp {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String original = sc.nextLine();
+        String input = sc.nextLine();
 
-        String reversed = "";
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        if (original.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("It is a Palindrome.");
         } else {
             System.out.println("It is NOT a Palindrome.");
@@ -28,5 +40,6 @@ public class PalindromeCheckerApp {
         sc.close();
     }
 }
+
 
 
