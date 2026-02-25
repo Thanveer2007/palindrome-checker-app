b1d826f38102ce52750cee063d483bec866c40a7
Thanveer Asshief <asshiefasshief@gmail.com>
2026-02-25 15:23:17 +0530

    uc2 - hardcode palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -9,11 +9,9 @@ public class PalindromeCheckerApp {
         String text = "madam";
 
         String reversed = "";
-
         for (int i = text.length() - 1; i >= 0; i--) {
             reversed += text.charAt(i);
         }
-
         if (text.equalsIgnoreCase(reversed)) {
             System.out.println(text + " is a Palindrome.");
         } else {
